b433462c3f495557240583916b1578543186fa5a
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2024-10-06 02:05:35 -0300

    crear prototipo basico de preparacion

--- TP_CAI/Forms/OrdenDePreparacion/Forms/OrdenDePreparacionForm.Designer.cs
+++ TP_CAI/Forms/OrdenDePreparacion/Forms/OrdenDePreparacionForm.Designer.cs
@@ -28,32 +28,286 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
+            comboBox1 = new ComboBox();
+            label2 = new Label();
+            comboBox2 = new ComboBox();
+            label3 = new Label();
+            ContinuarButton = new Button();
+            button2 = new Button();
+            ProductosGroup = new GroupBox();
+            EliminarProductoButton = new Button();
+            label6 = new Label();
+            ProductosAgregadosListView = new ListView();
+            AgregarProductoButton = new Button();
+            CantidadLabel = new Label();
+            CantidadTextBox = new TextBox();
+            label4 = new Label();
+            ProductosDisponiblesListView = new ListView();
+            InfoOrdenGroup = new GroupBox();
+            label8 = new Label();
+            label7 = new Label();
+            textBox2 = new TextBox();
+            PrioridadComboBox = new ComboBox();
+            CrearOrdenButton = new Button();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ProductosGroup.SuspendLayout();
+            InfoOrdenGroup.SuspendLayout();
             SuspendLayout();
             // 
-            // label1
+            // comboBox1
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(12, 9);
-            label1.Name = "label1";
-            label1.Size = new Size(333, 15);
-            label1.TabIndex = 0;
-            label1.Text = "Recepción de órdenes de preparación por parte de los clientes";
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "GrupoE S.R.L. CUIT 20-44444444-4. " });
+            comboBox1.Location = new Point(12, 26);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(387, 23);
+            comboBox1.TabIndex = 1;
             // 
-            // RecepcionOrdenesPreparacionForm
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 8);
+            label2.Name = "label2";
+            label2.Size = new Size(44, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Cliente";
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Items.AddRange(new object[] { "Deposito ID 5. Av. Cordoba 21211, CABA." });
+            comboBox2.Location = new Point(405, 26);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(306, 23);
+            comboBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(405, 8);
+            label3.Name = "label3";
+            label3.Size = new Size(54, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Depósito";
+            // 
+            // ContinuarButton
+            // 
+            ContinuarButton.Enabled = false;
+            ContinuarButton.Location = new Point(717, 26);
+            ContinuarButton.Name = "ContinuarButton";
+            ContinuarButton.Size = new Size(156, 23);
+            ContinuarButton.TabIndex = 5;
+            ContinuarButton.Text = "Continuar";
+            ContinuarButton.UseVisualStyleBackColor = true;
+            ContinuarButton.Click += ContinuarButton_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(12, 345);
+            button2.Name = "button2";
+            button2.Size = new Size(153, 23);
+            button2.TabIndex = 6;
+            button2.Text = "Volver al Menú Principal";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // ProductosGroup
+            // 
+            ProductosGroup.Controls.Add(EliminarProductoButton);
+            ProductosGroup.Controls.Add(label6);
+            ProductosGroup.Controls.Add(ProductosAgregadosListView);
+            ProductosGroup.Controls.Add(AgregarProductoButton);
+            ProductosGroup.Controls.Add(CantidadLabel);
+            ProductosGroup.Controls.Add(CantidadTextBox);
+            ProductosGroup.Controls.Add(label4);
+            ProductosGroup.Controls.Add(ProductosDisponiblesListView);
+            ProductosGroup.Location = new Point(12, 64);
+            ProductosGroup.Name = "ProductosGroup";
+            ProductosGroup.Size = new Size(861, 180);
+            ProductosGroup.TabIndex = 7;
+            ProductosGroup.TabStop = false;
+            ProductosGroup.Text = "Productos";
+            // 
+            // EliminarProductoButton
+            // 
+            EliminarProductoButton.Enabled = false;
+            EliminarProductoButton.Location = new Point(443, 137);
+            EliminarProductoButton.Name = "EliminarProductoButton";
+            EliminarProductoButton.Size = new Size(408, 29);
+            EliminarProductoButton.TabIndex = 7;
+            EliminarProductoButton.Text = "Eliminar producto seleccionado de la Orden";
+            EliminarProductoButton.UseVisualStyleBackColor = true;
+            EliminarProductoButton.Click += EliminarProductoButton_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(443, 19);
+            label6.Name = "label6";
+            label6.Size = new Size(260, 15);
+            label6.TabIndex = 6;
+            label6.Text = "Productos Agregados a la Orden de Preparación";
+            // 
+            // ProductosAgregadosListView
+            // 
+            ProductosAgregadosListView.Location = new Point(443, 37);
+            ProductosAgregadosListView.Name = "ProductosAgregadosListView";
+            ProductosAgregadosListView.Size = new Size(408, 97);
+            ProductosAgregadosListView.TabIndex = 5;
+            ProductosAgregadosListView.UseCompatibleStateImageBehavior = false;
+            ProductosAgregadosListView.View = View.Details;
+            // 
+            // AgregarProductoButton
+            // 
+            AgregarProductoButton.Enabled = false;
+            AgregarProductoButton.Location = new Point(243, 137);
+            AgregarProductoButton.Name = "AgregarProductoButton";
+            AgregarProductoButton.Size = new Size(171, 23);
+            AgregarProductoButton.TabIndex = 4;
+            AgregarProductoButton.Text = "Agregar Producto a la Orden";
+            AgregarProductoButton.UseVisualStyleBackColor = true;
+            AgregarProductoButton.Click += AgregarProductoButton_Click;
+            // 
+            // CantidadLabel
+            // 
+            CantidadLabel.AutoSize = true;
+            CantidadLabel.Location = new Point(6, 141);
+            CantidadLabel.Name = "CantidadLabel";
+            CantidadLabel.Size = new Size(110, 15);
+            CantidadLabel.TabIndex = 3;
+            CantidadLabel.Text = "Cantidad a agregar:";
+            // 
+            // CantidadTextBox
+            // 
+            CantidadTextBox.Enabled = false;
+            CantidadTextBox.Location = new Point(122, 137);
+            CantidadTextBox.Name = "CantidadTextBox";
+            CantidadTextBox.Size = new Size(115, 23);
+            CantidadTextBox.TabIndex = 2;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(6, 19);
+            label4.Name = "label4";
+            label4.Size = new Size(279, 15);
+            label4.TabIndex = 1;
+            label4.Text = "Productos Disponibles en el Depósito Seleccionado:";
+            // 
+            // ProductosDisponiblesListView
+            // 
+            ProductosDisponiblesListView.Location = new Point(6, 37);
+            ProductosDisponiblesListView.Name = "ProductosDisponiblesListView";
+            ProductosDisponiblesListView.Size = new Size(408, 97);
+            ProductosDisponiblesListView.TabIndex = 0;
+            ProductosDisponiblesListView.UseCompatibleStateImageBehavior = false;
+            ProductosDisponiblesListView.View = View.Details;
+            // 
+            // InfoOrdenGroup
+            // 
+            InfoOrdenGroup.Controls.Add(label8);
+            InfoOrdenGroup.Controls.Add(label7);
+            InfoOrdenGroup.Controls.Add(textBox2);
+            InfoOrdenGroup.Controls.Add(PrioridadComboBox);
+            InfoOrdenGroup.Location = new Point(12, 250);
+            InfoOrdenGroup.Name = "InfoOrdenGroup";
+            InfoOrdenGroup.Size = new Size(558, 79);
+            InfoOrdenGroup.TabIndex = 8;
+            InfoOrdenGroup.TabStop = false;
+            InfoOrdenGroup.Text = "Información de Orden de Preparación";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(272, 22);
+            label8.Name = "label8";
+            label8.Size = new Size(97, 15);
+            label8.TabIndex = 9;
+            label8.Text = "DNI Transportista";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(6, 22);
+            label7.Name = "label7";
+            label7.Size = new Size(55, 15);
+            label7.TabIndex = 10;
+            label7.Text = "Prioridad";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(272, 40);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(280, 23);
+            textBox2.TabIndex = 8;
+            // 
+            // PrioridadComboBox
+            // 
+            PrioridadComboBox.FormattingEnabled = true;
+            PrioridadComboBox.Items.AddRange(new object[] { "Baja", "Media", "Alta" });
+            PrioridadComboBox.Location = new Point(6, 40);
+            PrioridadComboBox.Name = "PrioridadComboBox";
+            PrioridadComboBox.Size = new Size(260, 23);
+            PrioridadComboBox.TabIndex = 9;
+            // 
+            // CrearOrdenButton
+            // 
+            CrearOrdenButton.Enabled = false;
+            CrearOrdenButton.Location = new Point(576, 257);
+            CrearOrdenButton.Name = "CrearOrdenButton";
+            CrearOrdenButton.Size = new Size(297, 72);
+            CrearOrdenButton.TabIndex = 9;
+            CrearOrdenButton.Text = "Crear Orden de Preparación";
+            CrearOrdenButton.UseVisualStyleBackColor = true;
+            CrearOrdenButton.Click += CrearOrdenButton_Click;
+            // 
+            // OrdenDePreparacionForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(label1);
-            Name = "RecepcionOrdenesPreparacionForm";
-            Text = "RecepcionOrdenesPreparacionForm";
+            ClientSize = new Size(880, 374);
+            Controls.Add(CrearOrdenButton);
+            Controls.Add(InfoOrdenGroup);
+            Controls.Add(ProductosGroup);
+            Controls.Add(button2);
+            Controls.Add(ContinuarButton);
+            Controls.Add(label3);
+            Controls.Add(comboBox2);
+            Controls.Add(label2);
+            Controls.Add(comboBox1);
+            Name = "OrdenDePreparacionForm";
+            Text = "Crear Orden de Preparación";
+            Load += OrdenDePreparacionForm_Load;
+            ProductosGroup.ResumeLayout(false);
+            ProductosGroup.PerformLayout();
+            InfoOrdenGroup.ResumeLayout(false);
+            InfoOrdenGroup.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private Label label1;
+        private ComboBox comboBox1;
+        private Label label2;
+        private ComboBox comboBox2;
+        private Label label3;
+        private Button ContinuarButton;
+        private Button button2;
+        private GroupBox ProductosGroup;
+        private Label label4;
+        private ListView ProductosDisponiblesListView;
+        private Label label6;
+        private ListView ProductosAgregadosListView;
+        private Button AgregarProductoButton;
+        private Button EliminarProductoButton;
+        private Label label5;
+        private TextBox CantidadTextBox;
+        private GroupBox InfoOrdenGroup;
+        private Label label8;
+        private Label label7;
+        private TextBox textBox2;
+        private ComboBox PrioridadComboBox;
+        private Button CrearOrdenButton;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Label CantidadLabel;
     }
 }
--- TP_CAI/Forms/OrdenDePreparacion/Forms/OrdenDePreparacionForm.cs
+++ TP_CAI/Forms/OrdenDePreparacion/Forms/OrdenDePreparacionForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace TP_CAI.Archivos.OrdenDePreparacion.Forms
 {
@@ -17,5 +18,177 @@ namespace TP_CAI.Archivos.OrdenDePreparacion.Forms
             InitializeComponent();
         }
 
+        private void OrdenDePreparacionForm_Load(object sender, EventArgs e)
+        {
+            // Desactivar el botón al cargar el formulario
+            ContinuarButton.Enabled = false;
+
+            // Desactivar el contenedor de Productos
+            ProductosGroup.Enabled = false;
+
+            InfoOrdenGroup.Enabled = false;
+
+            // Registrar los eventos SelectedIndexChanged de los ComboBox
+            comboBox1.SelectedIndexChanged += new EventHandler(ComboBox_SelectedIndexChanged);
+            comboBox2.SelectedIndexChanged += new EventHandler(ComboBox_SelectedIndexChanged);
+
+            // Registrar el evento SelectedIndexChanged del ListView
+            ProductosDisponiblesListView.SelectedIndexChanged += new EventHandler(ProductosDisponiblesListView_SelectedIndexChanged);
+
+            // Registrar el evento SelectedIndexChanged del ListView
+            ProductosAgregadosListView.SelectedIndexChanged += new EventHandler(ProductosAgregadosListView_SelectedIndexChanged);
+
+            // Registrar el evento TextChanged del CantidadTextBox
+            CantidadTextBox.TextChanged += new EventHandler(CantidadTextBox_TextChanged);
+
+            ProductosDisponiblesListView.FullRowSelect = true;
+            ProductosAgregadosListView.FullRowSelect = true;
+
+
+        }
+        private void ComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Verificar si ambos ComboBox tienen un valor seleccionado
+            if (comboBox1.SelectedItem != null && comboBox2.SelectedItem != null)
+            {
+                ContinuarButton.Enabled = true; // Habilitar el botón
+            }
+            else
+            {
+                ContinuarButton.Enabled = false; // Deshabilitar el botón
+            }
+        }
+
+        private void ProductosDisponiblesListView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Verificar si hay un elemento seleccionado en el ListView
+            if (ProductosDisponiblesListView.SelectedItems.Count > 0)
+            {
+                CantidadTextBox.Enabled = true; // Habilitar el TextBox
+            }
+            else
+            {
+                EliminarProductoButton.Enabled = false; // Deshabilitar el TextBox
+            }
+        }
+
+        private void ProductosAgregadosListView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Verificar si hay un elemento seleccionado en el ListView
+            if (ProductosAgregadosListView.SelectedItems.Count > 0)
+            {
+                EliminarProductoButton.Enabled = true; // Habilitar el TextBox
+            }
+            else
+            {
+                CantidadTextBox.Enabled = false; // Deshabilitar el TextBox
+            }
+        }
+
+        private void CantidadTextBox_TextChanged(object sender, EventArgs e)
+        {
+            // Verificar si el valor ingresado es un número válido
+            if (int.TryParse(CantidadTextBox.Text, out int cantidad) && cantidad > 0)
+            {
+                AgregarProductoButton.Enabled = true; // Habilitar el botón si es un número positivo
+            }
+            else
+            {
+                AgregarProductoButton.Enabled = false; // Deshabilitar el botón si el valor no es válido
+            }
+        }
+
+        private void ContinuarButton_Click(object sender, EventArgs e)
+        {
+            ProductosGroup.Enabled = true;
+
+            ListViewItem item1 = new ListViewItem(new[] { "1", "Remeras", "10" });
+            ListViewItem item2 = new ListViewItem(new[] { "2", "Zapatillas", "20" });
+            ListViewItem item3 = new ListViewItem(new[] { "3", "Pantalones", "30" });
+
+            ProductosDisponiblesListView.Columns.Add("ID", 70); // Ancho de la columna 50
+            ProductosDisponiblesListView.Columns.Add("Descripción", 170);
+            ProductosDisponiblesListView.Columns.Add("Unidades disponibles", 100);
+
+            ProductosAgregadosListView.Columns.Add("ID", 70); // Ancho de la columna 50
+            ProductosAgregadosListView.Columns.Add("Descripción", 170);
+            ProductosAgregadosListView.Columns.Add("Unidades agregadas", 100);
+
+            ProductosDisponiblesListView.Items.Add(item1);
+            ProductosDisponiblesListView.Items.Add(item2);
+            ProductosDisponiblesListView.Items.Add(item3);
+        }
+
+        private void AgregarProductoButton_Click(object sender, EventArgs e)
+        {
+            // Verificar si hay un elemento seleccionado en la lista original (listView1)
+            if (ProductosDisponiblesListView.SelectedItems.Count > 0 && int.TryParse(CantidadTextBox.Text, out int cantidad) && cantidad > 0)
+            {
+                // Obtener el elemento seleccionado
+                ListViewItem itemSeleccionado = ProductosDisponiblesListView.SelectedItems[0];
+
+                // Obtener los valores de las columnas (ID y Descripción) del item seleccionado
+                string id = itemSeleccionado.SubItems[0].Text;  // Columna ID
+                string descripcion = itemSeleccionado.SubItems[1].Text;  // Columna Descripción
+
+                // Crear un nuevo ListViewItem con los valores y la cantidad especificada
+                ListViewItem nuevoItem = new ListViewItem(new[] { id, descripcion, cantidad.ToString() });
+
+                // Agregar el nuevo item a la lista de productos agregados (ProductosAgregadosListView)
+                ProductosAgregadosListView.Items.Add(nuevoItem);
+
+                // Eliminar el item de la lista original (listView1)
+                ProductosDisponiblesListView.Items.Remove(itemSeleccionado);
+
+                // Limpiar el campo de cantidad
+                CantidadTextBox.Clear();
+
+                // Deshabilitar el botón nuevamente
+                AgregarProductoButton.Enabled = false;
+            }
+            else
+            {
+                MessageBox.Show("Por favor, selecciona un producto y una cantidad válida.");
+            }
+
+            InfoOrdenGroup.Enabled = true;
+            CrearOrdenButton.Enabled = true;
+        }
+
+        private void EliminarProductoButton_Click(object sender, EventArgs e)
+        {
+            // Verificar si hay un elemento seleccionado en la lista de productos agregados
+            if (ProductosAgregadosListView.SelectedItems.Count > 0)
+            {
+                // Obtener el elemento seleccionado
+                ListViewItem itemSeleccionado = ProductosAgregadosListView.SelectedItems[0];
+
+                // Obtener los valores de las columnas (ID y Descripción) del item seleccionado
+                string id = itemSeleccionado.SubItems[0].Text;  // Columna ID
+                string descripcion = itemSeleccionado.SubItems[1].Text;  // Columna Descripción
+                string cantidad = "10";
+
+                // Crear un nuevo ListViewItem para la lista original
+                ListViewItem nuevoItem = new ListViewItem(new[] { id, descripcion, cantidad });
+
+                // Agregar el item de vuelta a la lista original (listView1)
+                ProductosDisponiblesListView.Items.Add(nuevoItem);
+
+                // Eliminar el item de la lista de productos agregados (ProductosAgregadosListView)
+                ProductosAgregadosListView.Items.Remove(itemSeleccionado);
+
+                // Deshabilitar el botón de eliminar si ya no hay más elementos seleccionados
+                EliminarProductoButton.Enabled = false;
+            }
+            else
+            {
+                MessageBox.Show("Por favor, selecciona un producto para eliminar.");
+            }
+        }
+
+        private void CrearOrdenButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Orden Creada Satisfactoriamente\". ID de Orden: 004. Fecha de emisión: 06/10/2024 14:50hs");
+        }
     }
 }
