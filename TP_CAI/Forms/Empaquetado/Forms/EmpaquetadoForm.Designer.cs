fecf8c441b5e74f8ec888736d6c5bdb260a42c59
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2024-10-07 22:05:46 -0300

    agregar nuevo caso de uso

--- TP_CAI/Forms/Empaquetado/Forms/EmpaquetadoForm.Designer.cs
+++ TP_CAI/Forms/Empaquetado/Forms/EmpaquetadoForm.Designer.cs
@@ -38,18 +38,20 @@
             // 
             // LimpiarButton
             // 
-            LimpiarButton.Location = new Point(12, 220);
+            LimpiarButton.Location = new Point(17, 367);
+            LimpiarButton.Margin = new Padding(4, 5, 4, 5);
             LimpiarButton.Name = "LimpiarButton";
-            LimpiarButton.Size = new Size(75, 23);
+            LimpiarButton.Size = new Size(107, 38);
             LimpiarButton.TabIndex = 10;
             LimpiarButton.Text = "Limpiar";
             LimpiarButton.UseVisualStyleBackColor = true;
             // 
             // VolverAlMenuButton
             // 
-            VolverAlMenuButton.Location = new Point(12, 272);
+            VolverAlMenuButton.Location = new Point(17, 453);
+            VolverAlMenuButton.Margin = new Padding(4, 5, 4, 5);
             VolverAlMenuButton.Name = "VolverAlMenuButton";
-            VolverAlMenuButton.Size = new Size(144, 23);
+            VolverAlMenuButton.Size = new Size(206, 38);
             VolverAlMenuButton.TabIndex = 9;
             VolverAlMenuButton.Text = "Volver al Menú Principal";
             VolverAlMenuButton.UseVisualStyleBackColor = true;
@@ -58,18 +60,21 @@
             // OrdenesPreparacionGridView
             // 
             OrdenesPreparacionGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            OrdenesPreparacionGridView.Location = new Point(12, 30);
+            OrdenesPreparacionGridView.Location = new Point(17, 50);
+            OrdenesPreparacionGridView.Margin = new Padding(4, 5, 4, 5);
             OrdenesPreparacionGridView.Name = "OrdenesPreparacionGridView";
-            OrdenesPreparacionGridView.Size = new Size(580, 184);
+            OrdenesPreparacionGridView.RowHeadersWidth = 62;
+            OrdenesPreparacionGridView.Size = new Size(829, 307);
             OrdenesPreparacionGridView.TabIndex = 8;
             // 
             // GenerarOrdenButton
             // 
-            GenerarOrdenButton.Location = new Point(93, 220);
+            GenerarOrdenButton.Location = new Point(133, 367);
+            GenerarOrdenButton.Margin = new Padding(4, 5, 4, 5);
             GenerarOrdenButton.Name = "GenerarOrdenButton";
-            GenerarOrdenButton.Size = new Size(499, 25);
+            GenerarOrdenButton.Size = new Size(713, 42);
             GenerarOrdenButton.TabIndex = 7;
-            GenerarOrdenButton.Text = "Empaquetar";
+            GenerarOrdenButton.Text = "Seleccionar";
             GenerarOrdenButton.UseVisualStyleBackColor = true;
             GenerarOrdenButton.Click += GenerarOrdenButton_Click;
             // 
@@ -77,24 +82,27 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 12F);
-            label1.Location = new Point(12, 6);
+            label1.Location = new Point(17, 10);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(257, 21);
+            label1.Size = new Size(367, 32);
             label1.TabIndex = 6;
-            label1.Text = "Seleccionar órdenes de preparación";
+            label1.Text = "Seleccionar órdenes de selección";
+            label1.Click += label1_Click;
             // 
             // EmpaquetadoForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(606, 303);
+            ClientSize = new Size(866, 505);
             Controls.Add(LimpiarButton);
             Controls.Add(VolverAlMenuButton);
             Controls.Add(OrdenesPreparacionGridView);
             Controls.Add(GenerarOrdenButton);
             Controls.Add(label1);
+            Margin = new Padding(4, 5, 4, 5);
             Name = "EmpaquetadoForm";
-            Text = "Empaquetar";
+            Text = "Gestionar Orden de Selección";
             Load += EmpaquetadoForm_Load;
             ((System.ComponentModel.ISupportInitialize)OrdenesPreparacionGridView).EndInit();
             ResumeLayout(false);
--- TP_CAI/Forms/PantallaPrincipal/Forms/PantallaPrincipalForm.Designer.cs
+++ TP_CAI/Forms/PantallaPrincipal/Forms/PantallaPrincipalForm.Designer.cs
@@ -35,29 +35,33 @@
             DespachoButton = new Button();
             EmpaquetadoButton = new Button();
             CrearOrdenDeEntrega = new Button();
+            button1 = new Button();
             SuspendLayout();
             // 
             // RolUsuario
             // 
             RolUsuario.AutoSize = true;
-            RolUsuario.Location = new Point(134, 24);
+            RolUsuario.Location = new Point(191, 40);
+            RolUsuario.Margin = new Padding(4, 0, 4, 0);
             RolUsuario.Name = "RolUsuario";
-            RolUsuario.Size = new Size(0, 15);
+            RolUsuario.Size = new Size(0, 25);
             RolUsuario.TabIndex = 3;
             // 
             // NombreUsuario
             // 
             NombreUsuario.AutoSize = true;
-            NombreUsuario.Location = new Point(134, 9);
+            NombreUsuario.Location = new Point(191, 15);
+            NombreUsuario.Margin = new Padding(4, 0, 4, 0);
             NombreUsuario.Name = "NombreUsuario";
-            NombreUsuario.Size = new Size(0, 15);
+            NombreUsuario.Size = new Size(0, 25);
             NombreUsuario.TabIndex = 2;
             // 
             // RecepcionOrdenesPreparacionButton
             // 
-            RecepcionOrdenesPreparacionButton.Location = new Point(12, 12);
+            RecepcionOrdenesPreparacionButton.Location = new Point(17, 20);
+            RecepcionOrdenesPreparacionButton.Margin = new Padding(4, 5, 4, 5);
             RecepcionOrdenesPreparacionButton.Name = "RecepcionOrdenesPreparacionButton";
-            RecepcionOrdenesPreparacionButton.Size = new Size(226, 45);
+            RecepcionOrdenesPreparacionButton.Size = new Size(323, 75);
             RecepcionOrdenesPreparacionButton.TabIndex = 4;
             RecepcionOrdenesPreparacionButton.Text = "Crear Orden de Preparación";
             RecepcionOrdenesPreparacionButton.UseVisualStyleBackColor = true;
@@ -65,9 +69,10 @@
             // 
             // GeneracionOrdenesSeleccionButton
             // 
-            GeneracionOrdenesSeleccionButton.Location = new Point(12, 63);
+            GeneracionOrdenesSeleccionButton.Location = new Point(17, 105);
+            GeneracionOrdenesSeleccionButton.Margin = new Padding(4, 5, 4, 5);
             GeneracionOrdenesSeleccionButton.Name = "GeneracionOrdenesSeleccionButton";
-            GeneracionOrdenesSeleccionButton.Size = new Size(226, 45);
+            GeneracionOrdenesSeleccionButton.Size = new Size(323, 75);
             GeneracionOrdenesSeleccionButton.TabIndex = 5;
             GeneracionOrdenesSeleccionButton.Text = "Crear Orden de Selección";
             GeneracionOrdenesSeleccionButton.UseVisualStyleBackColor = true;
@@ -75,9 +80,10 @@
             // 
             // DespachoButton
             // 
-            DespachoButton.Location = new Point(12, 216);
+            DespachoButton.Location = new Point(17, 445);
+            DespachoButton.Margin = new Padding(4, 5, 4, 5);
             DespachoButton.Name = "DespachoButton";
-            DespachoButton.Size = new Size(226, 45);
+            DespachoButton.Size = new Size(323, 75);
             DespachoButton.TabIndex = 7;
             DespachoButton.Text = "Despacho";
             DespachoButton.UseVisualStyleBackColor = true;
@@ -85,9 +91,10 @@
             // 
             // EmpaquetadoButton
             // 
-            EmpaquetadoButton.Location = new Point(12, 114);
+            EmpaquetadoButton.Location = new Point(17, 275);
+            EmpaquetadoButton.Margin = new Padding(4, 5, 4, 5);
             EmpaquetadoButton.Name = "EmpaquetadoButton";
-            EmpaquetadoButton.Size = new Size(226, 45);
+            EmpaquetadoButton.Size = new Size(323, 75);
             EmpaquetadoButton.TabIndex = 6;
             EmpaquetadoButton.Text = "Empaquetado";
             EmpaquetadoButton.UseVisualStyleBackColor = true;
@@ -95,19 +102,31 @@
             // 
             // CrearOrdenDeEntrega
             // 
-            CrearOrdenDeEntrega.Location = new Point(12, 165);
+            CrearOrdenDeEntrega.Location = new Point(17, 360);
+            CrearOrdenDeEntrega.Margin = new Padding(4, 5, 4, 5);
             CrearOrdenDeEntrega.Name = "CrearOrdenDeEntrega";
-            CrearOrdenDeEntrega.Size = new Size(226, 45);
+            CrearOrdenDeEntrega.Size = new Size(323, 75);
             CrearOrdenDeEntrega.TabIndex = 8;
             CrearOrdenDeEntrega.Text = "Crear Orden de Entrega";
             CrearOrdenDeEntrega.UseVisualStyleBackColor = true;
             CrearOrdenDeEntrega.Click += CrearOrdenDeEntrega_Click;
             // 
+            // button1
+            // 
+            button1.Location = new Point(13, 190);
+            button1.Margin = new Padding(4, 5, 4, 5);
+            button1.Name = "button1";
+            button1.Size = new Size(323, 75);
+            button1.TabIndex = 9;
+            button1.Text = "Gestion Orden de Seleccion";
+            button1.UseVisualStyleBackColor = true;
+            // 
             // PantallaPrincipalForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(250, 277);
+            ClientSize = new Size(357, 544);
+            Controls.Add(button1);
             Controls.Add(CrearOrdenDeEntrega);
             Controls.Add(DespachoButton);
             Controls.Add(EmpaquetadoButton);
@@ -115,6 +134,7 @@
             Controls.Add(RecepcionOrdenesPreparacionButton);
             Controls.Add(RolUsuario);
             Controls.Add(NombreUsuario);
+            Margin = new Padding(4, 5, 4, 5);
             Name = "PantallaPrincipalForm";
             Text = "PantallaPrincipalForm";
             Load += PantallaPrincipalForm_Load;
@@ -130,5 +150,6 @@
         private Button DespachoButton;
         private Button EmpaquetadoButton;
         private Button CrearOrdenDeEntrega;
+        private Button button1;
     }
 }
