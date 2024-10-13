0d60716906692a42bdbdeefa3751542183c08477
PABLO MARTIN LOPEZ <pablomartinlopez11@gmail.com>
2024-10-13 15:20:47 -0300

    Agregar busqueda segun ID y listview para crear orden de entrega

--- TP_CAI/Forms/OrdenDeEntrega/Forms/OrdenDeEntregaForm.Designer.cs
+++ TP_CAI/Forms/OrdenDeEntrega/Forms/OrdenDeEntregaForm.Designer.cs
@@ -30,13 +30,14 @@
 		{
 			LimpiarButton = new Button();
 			VolverAlMenuButton = new Button();
-			OrdenesPreparacionGridView = new DataGridView();
 			GenerarOrdenButton = new Button();
-			SeleccionarTransporistaComboBox = new ComboBox();
-			label2 = new Label();
-			SeleccionarButton = new Button();
 			OrdenesGroupBox = new GroupBox();
-			((System.ComponentModel.ISupportInitialize)OrdenesPreparacionGridView).BeginInit();
+			BuscarButtton = new Button();
+			listView1 = new ListView();
+			textBox1 = new TextBox();
+			BuscarOrdenlbl = new Label();
+			IdOrden = new ColumnHeader();
+			FechaEntrega = new ColumnHeader();
 			OrdenesGroupBox.SuspendLayout();
 			SuspendLayout();
 			// 
@@ -52,7 +53,7 @@
 			// 
 			// VolverAlMenuButton
 			// 
-			VolverAlMenuButton.Location = new Point(12, 329);
+			VolverAlMenuButton.Location = new Point(12, 311);
 			VolverAlMenuButton.Name = "VolverAlMenuButton";
 			VolverAlMenuButton.Size = new Size(144, 23);
 			VolverAlMenuButton.TabIndex = 14;
@@ -60,82 +61,88 @@
 			VolverAlMenuButton.UseVisualStyleBackColor = true;
 			VolverAlMenuButton.Click += VolverAlMenuButton_Click;
 			// 
-			// OrdenesPreparacionGridView
-			// 
-			OrdenesPreparacionGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			OrdenesPreparacionGridView.Enabled = false;
-			OrdenesPreparacionGridView.Location = new Point(6, 22);
-			OrdenesPreparacionGridView.Name = "OrdenesPreparacionGridView";
-			OrdenesPreparacionGridView.Size = new Size(568, 184);
-			OrdenesPreparacionGridView.TabIndex = 13;
-			// 
 			// GenerarOrdenButton
 			// 
 			GenerarOrdenButton.Location = new Point(87, 210);
 			GenerarOrdenButton.Name = "GenerarOrdenButton";
-			GenerarOrdenButton.Size = new Size(487, 25);
+			GenerarOrdenButton.Size = new Size(271, 25);
 			GenerarOrdenButton.TabIndex = 12;
 			GenerarOrdenButton.Text = "Crear orden de entrega";
 			GenerarOrdenButton.UseVisualStyleBackColor = true;
 			GenerarOrdenButton.Click += GenerarOrdenButton_Click;
 			// 
-			// SeleccionarTransporistaComboBox
-			// 
-			SeleccionarTransporistaComboBox.FormattingEnabled = true;
-			SeleccionarTransporistaComboBox.Items.AddRange(new object[] { "DNI Transportista: 44444444", "DNI Transportista: 55555555", "DNI Transportista: 33333333" });
-			SeleccionarTransporistaComboBox.Location = new Point(12, 28);
-			SeleccionarTransporistaComboBox.Name = "SeleccionarTransporistaComboBox";
-			SeleccionarTransporistaComboBox.Size = new Size(427, 23);
-			SeleccionarTransporistaComboBox.TabIndex = 16;
-			// 
-			// label2
-			// 
-			label2.AutoSize = true;
-			label2.Location = new Point(12, 10);
-			label2.Name = "label2";
-			label2.Size = new Size(137, 15);
-			label2.TabIndex = 17;
-			label2.Text = "Seleccionar Transportista";
-			// 
-			// SeleccionarButton
-			// 
-			SeleccionarButton.Enabled = false;
-			SeleccionarButton.Location = new Point(445, 28);
-			SeleccionarButton.Name = "SeleccionarButton";
-			SeleccionarButton.Size = new Size(147, 23);
-			SeleccionarButton.TabIndex = 18;
-			SeleccionarButton.Text = "Seleccionar";
-			SeleccionarButton.UseVisualStyleBackColor = true;
-			SeleccionarButton.Click += SeleccionarButton_Click;
-			// 
 			// OrdenesGroupBox
 			// 
-			OrdenesGroupBox.Controls.Add(OrdenesPreparacionGridView);
+			OrdenesGroupBox.Controls.Add(listView1);
 			OrdenesGroupBox.Controls.Add(LimpiarButton);
 			OrdenesGroupBox.Controls.Add(GenerarOrdenButton);
 			OrdenesGroupBox.Enabled = false;
 			OrdenesGroupBox.Location = new Point(12, 57);
 			OrdenesGroupBox.Name = "OrdenesGroupBox";
-			OrdenesGroupBox.Size = new Size(580, 248);
+			OrdenesGroupBox.Size = new Size(358, 248);
 			OrdenesGroupBox.TabIndex = 19;
 			OrdenesGroupBox.TabStop = false;
 			OrdenesGroupBox.Text = "Ordenes de Preparación";
+			// 
+			// BuscarButtton
+			// 
+			BuscarButtton.Location = new Point(295, 28);
+			BuscarButtton.Name = "BuscarButtton";
+			BuscarButtton.Size = new Size(75, 23);
+			BuscarButtton.TabIndex = 20;
+			BuscarButtton.Text = "Buscar";
+			BuscarButtton.UseVisualStyleBackColor = true;
+			// 
+			// listView1
+			// 
+			listView1.Columns.AddRange(new ColumnHeader[] { IdOrden, FechaEntrega });
+			listView1.Location = new Point(6, 22);
+			listView1.Name = "listView1";
+			listView1.Size = new Size(352, 182);
+			listView1.TabIndex = 21;
+			listView1.UseCompatibleStateImageBehavior = false;
+			listView1.View = View.Details;
+			// 
+			// textBox1
+			// 
+			textBox1.Location = new Point(18, 28);
+			textBox1.Name = "textBox1";
+			textBox1.Size = new Size(271, 23);
+			textBox1.TabIndex = 22;
+			// 
+			// BuscarOrdenlbl
+			// 
+			BuscarOrdenlbl.AutoSize = true;
+			BuscarOrdenlbl.Location = new Point(18, 9);
+			BuscarOrdenlbl.Name = "BuscarOrdenlbl";
+			BuscarOrdenlbl.Size = new Size(136, 15);
+			BuscarOrdenlbl.TabIndex = 23;
+			BuscarOrdenlbl.Text = "ID Orden de Preparacion";
+			// 
+			// IdOrden
+			// 
+			IdOrden.Text = "ID Orden";
+			IdOrden.Width = 150;
+			// 
+			// FechaEntrega
+			// 
+			FechaEntrega.Text = "Fecha de entrega";
+			FechaEntrega.Width = 200;
 			// 
 			// OrdenDeEntregaForm
 			// 
 			AutoScaleDimensions = new SizeF(7F, 15F);
 			AutoScaleMode = AutoScaleMode.Font;
-			ClientSize = new Size(607, 364);
+			ClientSize = new Size(378, 341);
+			Controls.Add(BuscarOrdenlbl);
+			Controls.Add(textBox1);
+			Controls.Add(BuscarButtton);
 			Controls.Add(OrdenesGroupBox);
-			Controls.Add(SeleccionarButton);
-			Controls.Add(label2);
-			Controls.Add(SeleccionarTransporistaComboBox);
 			Controls.Add(VolverAlMenuButton);
 			MaximizeBox = false;
 			Name = "OrdenDeEntregaForm";
 			Text = "Crear Orden de Entrega";
 			Load += OrdenDeEntregaForm_Load;
-			((System.ComponentModel.ISupportInitialize)OrdenesPreparacionGridView).EndInit();
 			OrdenesGroupBox.ResumeLayout(false);
 			ResumeLayout(false);
 			PerformLayout();
@@ -145,11 +152,13 @@
 
 		private Button LimpiarButton;
         private Button VolverAlMenuButton;
-        private DataGridView OrdenesPreparacionGridView;
         private Button GenerarOrdenButton;
-        private ComboBox SeleccionarTransporistaComboBox;
-        private Label label2;
-        private Button SeleccionarButton;
         private GroupBox OrdenesGroupBox;
-    }
+		private TextBox textBox1;
+		private ListView listView1;
+		private Button BuscarButtton;
+		private Label BuscarOrdenlbl;
+		private ColumnHeader IdOrden;
+		private ColumnHeader FechaEntrega;
+	}
 }
