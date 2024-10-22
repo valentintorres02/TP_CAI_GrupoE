a0914e9bb83ab967155973f1a4af928f888ff82e
PABLO MARTIN LOPEZ <pablomartinlopez11@gmail.com>
2024-10-22 19:24:26 -0300

    Almacen orden de entrega y almacen mercaderias

---
+++ TP_CAI/Almacenes/MercaderiaAlmacen.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TP_CAI.Almacenes
+{
+	internal static class MercaderiaAlmacen
+	{
+		private static List<MercaderiaEntidad> mercaderia = new List<MercaderiaEntidad>();
+
+		public static IReadOnlyCollection<MercaderiaEntidad> Mercaderia => mercaderia.AsReadOnly();
+
+
+
+		public static void Grabar()
+		{
+			var datos = JsonSerializer.Serialize(mercaderia);
+
+			File.WriteAllText("Mercaderia.json", datos);
+		}
+
+		public static void Leer()
+		{
+			if (!File.Exists("Mercaderia.json"))
+			{
+				return;
+			}
+
+			var datos = File.ReadAllText("Mercaderia.json");
+
+			mercaderia = JsonSerializer.Deserialize<List<MercaderiaEntidad>>(datos)!;
+		}
+
+		//FALTA AGREGAR EN EL PROGRAM SE EJECUTEN GRABAR Y LEER
+
+
+	}
+}
---
+++ TP_CAI/Almacenes/OrdenDeEntregaAlmacen.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TP_CAI.Almacenes
+{
+	internal static class OrdenDeEntregaAlmacen
+	{
+		private static List<OrdenDeEntregaEntidad> ordenDeEntrega = new List<OrdenDeEntregaEntidad>();
+
+		public static IReadOnlyCollection<OrdenDeEntregaEntidad> OrdenDeEntrega => ordenDeEntrega.AsReadOnly();
+
+
+
+		public static void Grabar()
+		{
+			var datos = JsonSerializer.Serialize(ordenDeEntrega);
+
+			File.WriteAllText("OrdenDeEntrega.json", datos);
+		}
+
+		public static void Leer()
+		{
+			if (!File.Exists("OrdenDeEntrega.json"))
+			{
+				return;
+			}
+
+			var datos = File.ReadAllText("OrdenDeEntrega.json");
+
+			ordenDeEntrega = JsonSerializer.Deserialize<List<OrdenDeEntregaEntidad>>(datos)!;
+		}
+
+		//FALTA AGREGAR EN EL PROGRAM SE EJECUTEN GRABAR Y LEER
+
+
+	}
+}
