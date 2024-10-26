3b56d877d65baaa5c4f64218c8c2ab9cbae2d8ff
PABLO MARTIN LOPEZ <pablomartinlopez11@gmail.com>
2024-10-25 22:18:12 -0300

    Creacion de clienteEntidad y clienteAlmacen

---
+++ TP_CAI/Almacenes/ClienteAlmacen.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TP_CAI.Almacenes
+{
+	internal class ClienteAlmacen
+	{
+		private static List<ClienteEntidad> clientes = [];
+
+		public static IReadOnlyCollection<ClienteEntidad> Clientes => clientes.AsReadOnly();
+
+		public static void Grabar()
+		{
+			var datos = JsonSerializer.Serialize(clientes);
+			File.WriteAllText("Clientes.json", datos);
+		}
+
+		public static void Leer()
+		{
+			if (!File.Exists(@"Clientes.json"))
+			{
+				return;
+			}
+
+			var datos = File.ReadAllText("Clientes.json");
+
+			clientes = JsonSerializer.Deserialize<List<ClienteEntidad>>(datos)!;
+		}
+	}
+}
---
+++ TP_CAI/Almacenes/ClienteEntidad.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_CAI.Almacenes
+{
+	public class ClienteEntidad
+	{
+		public int ClienteId { get; set; }
+		public string CUIT { get; set; }
+		public string Nombre { get; set; }
+	}
+}
